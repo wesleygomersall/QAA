1f84a28f9d61fae4e5a0f3592c0794907d62f7a4
wesleygomersall <gomersallw@gmail.com>
2024-09-09 23:55:42 -0700

    staralign.sh is completed

--- staralign.sh
+++ staralign.sh
@@ -15,15 +15,11 @@ S1_READ2=/projects/bgmp/wesg/bioinfo/Bi623/QAA/27_4C_mbnl_S19_L008.R2.cut.trim.f
 S2_READ1=/projects/bgmp/wesg/bioinfo/Bi623/QAA/32_4G_both_S23_L008.R1.cut.trim.fastq.gz
 S2_READ2=/projects/bgmp/wesg/bioinfo/Bi623/QAA/32_4G_both_S23_L008.R2.cut.trim.fastq.gz
 
-Mus_musculus.GRCm39.dna.primary_assembly.fa.gz
-Mus_musculus.GRCm39.112.gtf
-
 /usr/bin/time -v STAR --runThreadN 8 --runMode genomeGenerate \
 	--genomeDir Mus_musculus.GRCm39.dna_ens112_STAR2.7.10b \
-	--genomeFastaFiles mmu/Mus_musculus.GRCm39.dna.primary_assembly.fa.gz \
+	--genomeFastaFiles mmu/Mus_musculus.GRCm39.dna.primary_assembly.fa \
 	--sjdbGTFfile Mus_musculus.GRCm39.112.gtf
 
-# Need to copy this for sample 2
 /usr/bin/time -v STAR --runThreadN 8 --runMode alignReads \
 	--outFilterMultimapNmax 3 \
 	--outSAMunmapped Within KeepPairs \
@@ -33,10 +29,26 @@ Mus_musculus.GRCm39.112.gtf
 	--genomeDir Mus_musculus.GRCm39.dna_ens112_STAR2.7.10b \
 	--outFileNamePrefix ./mmu/27_
 
+/usr/bin/time -v STAR --runThreadN 8 --runMode alignReads \
+	--outFilterMultimapNmax 3 \
+	--outSAMunmapped Within KeepPairs \
+	--alignIntronMax 1000000 --alignMatesGapMax 1000000 \
+	--readFilesCommand zcat \
+	--readFilesIn $S2_READ1 $S2_READ2 \
+	--genomeDir Mus_musculus.GRCm39.dna_ens112_STAR2.7.10b \
+	--outFileNamePrefix ./mmu/32_
+
+/usr/bin/time -v htseq-count --stranded=yes mmu/27_Aligned.out.sam Mus_musculus.GRCm39.112.gtf
+/usr/bin/time -v htseq-count --stranded=reverse mmu/27_Aligned.out.sam Mus_musculus.GRCm39.112.gtf
+
+/usr/bin/time -v htseq-count --stranded=yes mmu/32_Aligned.out.sam Mus_musculus.GRCm39.112.gtf
+/usr/bin/time -v htseq-count --stranded=reverse mmu/32_Aligned.out.sam Mus_musculus.GRCm39.112.gtf
+
 conda deactivate
-conda activate bgmp_py312 #no python in the last env
+conda activate bgmp_py312 
 conda list 
 
-# ./countmappedSAM.py -i "./dre/Aligned.out.sam" # Part 2
+/usr/bin/time -v ./countmappedSAM.py -i mmu/27_Aligned.out.sam
+/usr/bin/time -v ./countmappedSAM.py -i mmu/32_Aligned.out.sam 
 
 conda deactivate
